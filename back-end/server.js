b44fde50b1348c3649d718be4c86d53c883bfc90
Franziska Kissling <frankissling@yahoo.co.uk>
2023-05-05 09:34:58 +0100

    Server.js packages and routes from captains log added

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,11 @@
-const message = 'Hi!'
+// Essential NPM packages
+const express = require('express');
+const mongoose = require('mongoose');
+const cores = require('cors');
+
+const port = process.env.PORT|| 5000;
+
+//Middleware
+app.use(express.json())
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
