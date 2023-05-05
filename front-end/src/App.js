4db1d1f7564a04a9658718659d4ad7823a98d5c9
Catherine Loesch <catherine.loesch@protonmail.com>
2023-05-05 12:05:13 +0100

    set up react-router and seleton components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './Components/Homepage/Homepage';
+import PageNotFound from './Components/PageNotFound/PageNotFound';
+import Posts from './Components/Posts/Posts';
+import Post from './Components/Posts/Post';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Project 3</h1>
+      <Routes>
+        <Route path='/' element={<Homepage />}></Route>
+        <Route path='/posts' element={<Posts />}></Route>
+        <Route path='/post/:id' element={<Post />}></Route>
+        <Route path='*' element={<PageNotFound />}></Route>
+      </Routes>
     </div>
   );
 }
