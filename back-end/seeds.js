4df9a451c5ff9fde9cf43e9b28ceb6fcad487041
Hari Ramanathan <hp.ramanathan@gmail.com>
2023-05-08 10:37:11 +0100

    Commit change before merge

--- back-end/seeds.js
+++ back-end/seeds.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const User = require("./models/user");
 const Post = require("./models/post");
 
-const mongoURI = "mongodb://localhost:27017/posts";
+const mongoURI = "mongodb://localhost:27017/divorce";
 
 mongoose.connect(mongoURI)
  .then(() => console.log("MongoDB Connected..."))
@@ -36,22 +36,22 @@ const fran = new User({
     posts: []
 })
 
-// Saving Four Users
-hari.save()
-  .then(() => console.log("user hari saved."))
-  .catch(() => console.log("user hari NOT saved."))
+// // Saving Four Users
+// hari.save()
+//   .then(() => console.log("user hari saved."))
+//   .catch(() => console.log("user hari NOT saved."))
 
-ben.save()
-  .then(() => console.log("user ben saved."))
-  .catch(() => console.log("user ben NOT saved."))
+// ben.save()
+//   .then(() => console.log("user ben saved."))
+//   .catch(() => console.log("user ben NOT saved."))
 
-katie.save()
-  .then(() => console.log("user katie saved."))
-  .catch(() => console.log("user katie NOT saved."))
+// katie.save()
+//   .then(() => console.log("user katie saved."))
+//   .catch(() => console.log("user katie NOT saved."))
 
-fran.save()
-  .then(() => console.log("user fran saved."))
-  .catch(() => console.log("user fran NOT saved."))
+// fran.save()
+//   .then(() => console.log("user fran saved."))
+//   .catch(() => console.log("user fran NOT saved."))
 
 
 // Create Guest Book Posts
@@ -61,22 +61,22 @@ const hariFirstPost = new Post({
     content: "Blah blah blah"
 }, { timestamps: Date.now })
 
-const hariSecondPost = new Post({
-  author: "Hari Ramanathan",
-  title: "Hari's Second Post",
-  content: "Yadda yadda yadda"
-}, { timestamps: Date.now })
+// const hariSecondPost = new Post({
+//   author: "Hari Ramanathan",
+//   title: "Hari's Second Post",
+//   content: "Yadda yadda yadda"
+// }, { timestamps: Date.now })
 
-const benFirstPost = new Post({
-  author: "Ben Khoury",
-  title: "Ben's Post",
-  content: "Bhooyakasha. It was the best of times. It was the worst of times."
-}, { timestamps: Date.now })
+// const benFirstPost = new Post({
+//   author: "Ben Khoury",
+//   title: "Ben's Post",
+//   content: "Bhooyakasha. It was the best of times. It was the worst of times."
+// }, { timestamps: Date.now })
 
 // Push Post into Posts Array
 hari.posts.push(hariFirstPost)
-hari.posts.push(hariSecondPost)
-ben.posts.push(benFirstPost)
+// hari.posts.push(hariSecondPost)
+// ben.posts.push(benFirstPost)
 
 // Save Posts Array
 hari.save()
