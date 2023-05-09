54b913c30c51cc66c88d107da6428530041db8b2
Catherine Loesch <catherine.loesch@protonmail.com>
2023-05-09 11:24:20 +0100

    generate secret key in .env file

--- back-end/lib/passportOptions.js
+++ back-end/lib/passportOptions.js
@@ -2,3 +2,6 @@ const passportJWT = require('passport-jwt')
 const ExtractJwt = passportJWT.ExtractJwt
 
 const jwtOptions = {}
+
+// jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+// jwtOptions.secretOrKey = process.env.SECRET_KEY
